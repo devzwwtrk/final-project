4deee5da633682cb7b3d0e3478ab0fc05338376b
devzwwtrk <poslezawwtrak@gmail.com>
2025-05-25 13:00:04 +0500

    SA1

---
+++ games/snake/game.js
@@ -0,0 +1,132 @@
+let intervalId = null;
+const ScoreEl = document.getElementById('score')
+const overlay = document.getElementById('overlay')
+
+const board = document.querySelector('canvas');
+const size = 25, colls = 25, rows = 30;
+board.width = size * rows;
+board.height = size * colls
+
+const c = board.getContext('2d');
+let gameRunning = false
+
+const snake = {
+    body: [{
+        x: Math.floor((rows/2)) * size,
+        y: Math.floor((colls/2)) * size,
+    }],    
+    direction: {dx: 1, dy:0},
+    length: 1
+}
+
+const food = {
+    x: undefined,
+    y: undefined,
+    placeFood: function() {
+        food.x = Math.floor(1 + Math.random() * (rows - 2)) * size;
+        food.y = Math.floor(1 + Math.random() * (colls - 2)) * size;
+    }
+};
+
+function startGame() {
+    resetGame();
+    food.placeFood();
+    intervalId = setInterval(draw, 100);
+    overlay.classList.remove('show');
+    gameRunning = true;
+}
+
+function resetGame() {
+    snake.body = [{
+        x: Math.floor((rows/2)) * size,
+        y: Math.floor((colls/2)) * size,
+    }];
+    snake.length = 1;
+    snake.direction = {dx: 1, dy: 0};
+    updateScore();
+}
+
+function updateScore() {
+    ScoreEl.textContent = `Score: ${snake.length}`;
+}
+
+function draw() {
+    c.clearRect(0, 0, board.width, board.height);
+            // drawing food
+    c.fillStyle = 'red';
+    c.fillRect(food.x, food.y, size, size);
+
+            // drawing snake
+    c.fillStyle = 'yellow';
+    for (let i = 0; i < snake.body.length; i++) {
+        const segment = snake.body[i];
+        c.fillRect(segment.x, segment.y, size, size);
+    };
+            // creating new head
+    const newHead = {
+        x: snake.body[0].x + (snake.direction.dx * size),
+        y: snake.body[0].y + (snake.direction.dy * size)
+    };
+
+    if (collision(newHead, snake.body)) {
+        clearInterval(intervalId);
+        gameOver();
+        return;
+    }
+            // adding new head in beginning of the body
+    snake.body.unshift(newHead);
+
+    if (snake.body[0].x == food.x && snake.body[0].y == food.y) {
+        snake.length++;
+        food.placeFood();
+        updateScore()
+    } else {
+        snake.body.pop();
+    }
+}
+
+function gameOver() {
+    overlay.innerHTML = `GAME OVER<br><span class="small">Press Space</span>`;
+    overlay.classList.add('show');
+    gameRunning = false;
+}
+            // snake hits the wall
+function collision(head, tail) {
+    if(head.x < 0 || head.y < 0 || head.x + size > board.width || head.y + size > board.height) {
+        return true;
+    }
+
+            // snake hits itself
+    for(let i = 0; i < tail.length; i++) {
+        if(head.x == tail[i].x && head.y == tail[i].y) {
+            return true;
+        }
+    }
+    return false;
+
+}
+
+
+document.addEventListener('keydown', (e)=> {
+    const dir = snake.direction;
+
+    if (!gameRunning && e.code == 'Space') {
+        startGame();
+    }
+
+
+    if ((e.key == 'ArrowLeft' || e.key == 'a') && dir.dx != 1) {
+        dir.dx = -1;
+        dir.dy = 0;
+    } else if ((e.key == 'ArrowRight' || e.key == 'd') && dir.dx != -1) {
+        dir.dx = 1;
+        dir.dy = 0;
+    } else if ((e.key == 'ArrowDown' || e.key == 's') && dir.dy != -1) {
+        dir.dx = 0;
+        dir.dy = 1;
+    } else if ((e.key == 'ArrowUp' || e.key == 'w') && dir.dy != 1) {
+        dir.dx = 0;
+        dir.dy = -1;
+    }
+});
+
